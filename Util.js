c6017c3563566edd0f63f57853207ada0e699de9
simba-luo <r.puppy@qq.com>
2020-06-17 08:21:48 +0800

    opt

--- Util.js
+++ Util.js
@@ -3,9 +3,17 @@
 const Util = function () {};
 
 Util.rnd = function (max) {
+
+    // ECMA 20.2 The Math Object
     return Math.floor(
+
+        // the range of `Math.random()' is [0,1)
+        // the range of `Math.random() * max' is [0, max)
         Math.random() * max
     );
+
+    // random in range [a, b):
+    // (double) Math.random() * (b - a) + a;
 };
 
 
